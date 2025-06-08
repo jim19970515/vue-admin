9b47e0dc9605ad03241335fb6f5131b5c26bea46
jim <f0920515972@gmail.com>
2025-06-08 14:24:03 +0800

    feat:安裝swal aleat

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite' // 动态配置函数
-import { createVuePlugin } from 'vite-plugin-vue2'
+import { defineConfig } from "vite"; // 动态配置函数
+import { createVuePlugin } from "vite-plugin-vue2";
 export default () =>
-    defineConfig({
-        plugins: [createVuePlugin()],
-        resolve: {
-            alias: [
-                {
-                    find: '@',
-                    replacement: '/src',
-                },
-            ],
-        },
-    })
+  defineConfig({
+    plugins: [createVuePlugin()],
+    resolve: {
+      alias: [
+        {
+          find: "@",
+          replacement: "/src/"
+        }
+      ]
+    }
+  });
